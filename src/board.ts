c7163334cf2b74be23fa1bf9b034a2d1aed3a65e
danilomsou <danilomsou@gmail.com>
2024-03-29 21:09:18 -0300

    feat: return only one color from enum

--- src/board.ts
+++ src/board.ts
@@ -2,13 +2,18 @@ export type Cell = {
   color: string;
 };
 
+export enum Color {
+  WHITE = "white",
+  BLACK = "black",
+}
+
 export class Board {
   rows: Array<Array<Cell>> = [];
 
   create() {
     this.rows = Array.from({ length: 8 }, (_, rowIndex) =>
       Array.from({ length: 8 }, (_, cellIndex) => ({
-        color: (rowIndex + cellIndex) % 2 === 0 ? "white" : "black",
+        color: (rowIndex + cellIndex) % 2 === 0 ? Color.WHITE : Color.BLACK,
       }))
     );
 
