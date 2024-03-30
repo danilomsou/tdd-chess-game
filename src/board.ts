2d518a13038eb00cb1d904544386879164e99fb9
danilomsou <danilomsou@gmail.com>
2024-03-29 22:07:01 -0300

    feat: create a board with 8 pawns for each player

--- src/board.ts
+++ src/board.ts
@@ -1,27 +1,53 @@
-export type Cell = {
-  color: Color;
-  row: number;
-  column: number;
-};
-
 export enum Color {
   WHITE = "white",
   BLACK = "black",
 }
+
+export enum PieceType {
+  PAWN = "pawn",
+}
+
+export type Piece = {
+  type: PieceType;
+  color: Color;
+};
+
+export type Cell = {
+  color: Color;
+  piece?: Piece;
+  row: number;
+  column: number;
+};
 
 export class Board {
   cells: Cell[] = [];
 
   constructor() {
     this.initializeBoard();
+    this.populatePawns();
   }
 
   private initializeBoard() {
     Array.from({ length: 8 }).forEach((_, row) => {
       Array.from({ length: 8 }).forEach((_, column) => {
         const color = this.isCellWhite(row, column) ? Color.WHITE : Color.BLACK;
-        this.cells.push({ color, row, column });
+        this.cells.push({
+          color,
+          row,
+          column,
+        });
       });
+    });
+  }
+
+  private populatePawns() {
+    Array.from({ length: 8 }).forEach((_, column) => {
+      this.cells.find(
+        (cell) => cell.row === 0 && cell.column === column
+      )!.piece = { type: PieceType.PAWN, color: Color.WHITE };
+      this.cells.find(
+        (cell) => cell.row === 2 && cell.column === column
+      )!.piece = { type: PieceType.PAWN, color: Color.BLACK };
     });
   }
 
