df96af19e420344153dd80d928a6ca46a20918d8
danilomsou <danilomsou@gmail.com>
2024-03-29 21:05:28 -0300

    test: get cell color by function

--- src/board.spec.ts
+++ src/board.spec.ts
@@ -1,18 +1,24 @@
 import { describe, expect, it } from "bun:test";
 
 import { Board } from "./board";
+import type { Cell } from "./board";
+
+const setUp = () => {
+  const board = new Board();
+  board.create();
+
+  return board;
+};
 
 describe("Board ", () => {
   it("should be able to create a new board", () => {
-    const board = new Board();
-    board.create();
+    const board = setUp();
+
     expect(board).toBeDefined();
   });
 
   it("should be able to create a new board with dimension 8x8 cells", () => {
-    const board = new Board();
-
-    board.create();
+    const board = setUp();
 
     expect(board.rows.length).toBe(8);
     board.rows.forEach((row) => {
@@ -21,9 +27,7 @@ describe("Board ", () => {
   });
 
   it("should have a white cell and an alternating black cell.", () => {
-    const board = new Board();
-
-    board.create();
+    const board = setUp();
 
     board.rows.forEach((row, rowIndex) => {
       row.forEach((cell, cellIndex) => {
@@ -37,11 +41,20 @@ describe("Board ", () => {
   });
 
   it("should create a board with a white cell at the bottom right for each player", () => {
-    const board = new Board();
-
-    board.create();
+    const board = setUp();
 
     expect(board.rows[7][7].color).toBe("white");
     expect(board.rows[0][0].color).toBe("white");
+  });
+
+  it("should be returns cell color", () => {
+    const board = setUp();
+
+    const cell = {
+      row: 0,
+      column: 0,
+    };
+
+    expect(board.getCellColor(cell)).toBe("white");
   });
 });
